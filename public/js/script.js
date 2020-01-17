6ea5231127abc9e0465523707a1e71a32365abfb
Mbdehaan <mbdehaan@outlook.com>
2020-01-17 14:29:05 +0100

    Various changes to html and sass code.
    Started working on the main content styling.

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,12 @@
+// bij klik op de menu_toggle , wordt de functie uitgevoerd
 document.getElementById('menu_toggle').onclick = function() {
+	//om de tekstkleur te veranderen naar wit
 	(this).classList.toggle('active');
+
+	//om het menu aan te geven dat hij uit moet klappen
     document.getElementById('mobile_menu').classList.toggle('active');
 
+	// tekst veranderen van menu naar close en terug
 	if (this.innerHTML === "MENU") {
 		this.innerHTML = "CLOSE";
 	} else {
