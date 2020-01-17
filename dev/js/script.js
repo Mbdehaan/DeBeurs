760e2282dc09af0fd4ca10616706f9165c959de3
Mbdehaan <mbdehaan@outlook.com>
2020-01-17 01:41:05 +0100

    Navigation styles
    Completed the navigation.
    It also scales correctly to desktop from 900px and above.
    Also various fixes to the sass code.

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,3 +1,10 @@
 document.getElementById('menu_toggle').onclick = function() {
+	(this).classList.toggle('active');
     document.getElementById('mobile_menu').classList.toggle('active');
-}
+
+	if (this.innerHTML === "MENU") {
+		this.innerHTML = "CLOSE";
+	} else {
+		this.innerHTML = "MENU";
+	}
+};
