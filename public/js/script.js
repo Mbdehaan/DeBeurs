06aa9ed2b4cce95002ec99c2e70a7538a4feff4d
Mbdehaan <mbdehaan@outlook.com>
2020-01-21 13:54:19 +0100

    Finished the homepage
    Finished the homepage and started on the second page - contact.

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,11 @@
-// bij klik op de menu_toggle , wordt de functie uitgevoerd
 document.getElementById('menu_toggle').onclick = function() {
-	//om de tekstkleur te veranderen naar wit
 	(this).classList.toggle('active');
-
-	//om het menu aan te geven dat hij uit moet klappen
     document.getElementById('mobile_menu').classList.toggle('active');
 
-	// tekst veranderen van menu naar close en terug
 	if (this.innerHTML === "MENU") {
 		this.innerHTML = "CLOSE";
 	} else {
 		this.innerHTML = "MENU";
 	}
 };
+
